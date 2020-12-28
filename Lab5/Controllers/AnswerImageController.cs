b886395d229bb91203cb636d1aa621e3d6c14b8f
Aria Gomes <aria.gomes00@gmail.com>
2020-12-27 22:16:20 -0500

    Init

---
+++ Lab5/Controllers/AnswerImageController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Storage.Blobs;
+using Lab5.Data;
+using Lab5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5.Controllers
+{
+    public class AnswerImageController : Controller
+    {
+        private readonly AnswerImageDataContext _context;
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string containerName = "answerimages";
+
+        public AnswerImageController(AnswerImageDataContext context, BlobServiceClient blobServiceClient)
+        {
+            _context = context;
+            _blobServiceClient = blobServiceClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.AnswerImage.ToListAsync());
+        }
+
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+
+            BlobContainerClient containerClient;
+            // Create the container and return a container client object
+            try
+            {
+                containerClient = await _blobServiceClient.CreateBlobContainerAsync(containerName);
+                containerClient.SetAccessPolicy(Azure.Storage.Blobs.Models.PublicAccessType.BlobContainer);
+            }
+            catch (RequestFailedException)
+            {
+                containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            }
+
+
+            try
+            {
+                // create the blob to hold the data
+                var blockBlob = containerClient.GetBlobClient(file.FileName);
+                if (await blockBlob.ExistsAsync())
+                {
+                    await blockBlob.DeleteAsync();
+                }
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    // copy the file data into memory
+                    await file.CopyToAsync(memoryStream);
+
+                    // navigate back to the beginning of the memory stream
+                    memoryStream.Position = 0;
+
+                    // send the file to the cloud
+                    await blockBlob.UploadAsync(memoryStream);
+                    memoryStream.Close();
+                }
+
+                // add the photo to the database if it uploaded successfully
+                var image = new AnswerImage();
+                image.Url = blockBlob.Uri.AbsoluteUri;
+                image.FileName = file.FileName;
+
+                _context.AnswerImage.Add(image);
+                _context.SaveChanges();
+            }
+            catch (RequestFailedException)
+            {
+                View("Error");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.AnswerImage
+                .FirstOrDefaultAsync(m => m.AnswerImageID == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var image = await _context.AnswerImage.FindAsync(id);
+
+
+            BlobContainerClient containerClient;
+            // Get the container and return a container client object
+            try
+            {
+                containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+            }
+            catch (RequestFailedException)
+            {
+                return View("Error");
+            }
+
+            try
+            {
+                // Get the blob that holds the data
+                var blockBlob = containerClient.GetBlobClient(image.FileName);
+                if (await blockBlob.ExistsAsync())
+                {
+                    await blockBlob.DeleteAsync();
+                }
+
+                _context.AnswerImage.Remove(image);
+                await _context.SaveChangesAsync();
+
+            }
+            catch (RequestFailedException)
+            {
+                return View("Error");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+   
+
+    }
+}
---
+++ Lab5/Data/AnswerImageDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab5.Models;
+
+namespace Lab5.Data
+{
+    public class AnswerImageDataContext : DbContext
+    {
+        
+
+        public AnswerImageDataContext(DbContextOptions<AnswerImageDataContext> options) : base(options)
+        {
+             
+        }
+        public DbSet<AnswerImage> AnswerImage { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnswerImage>().ToTable("AnswerImage");
+        }
+    }
+}
